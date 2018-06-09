0ea9114c44a2c72ae44ae26f0f0f2695b4a65025
Davide Gallitelli <davidegallitelli@gmail.com>
2018-06-09 17:00:33 +0200

    Updating DeezerMusicQuiz

---
+++ Labs/DeezerMusicQuiz/src/DeezerTrack.java
@@ -0,0 +1,76 @@
+public class DeezerTrack {
+    public long id;
+    public String title;
+    public String title_short;
+    public int duration;
+    public long rank;
+    public String releaseDate;
+    public String preview;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle_short() {
+        return title_short;
+    }
+
+    public void setTitle_short(String title_short) {
+        this.title_short = title_short;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public long getRank() {
+        return rank;
+    }
+
+    public void setRank(long rank) {
+        this.rank = rank;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPreview() {
+        return preview;
+    }
+
+    public void setPreview(String preview) {
+        this.preview = preview;
+    }
+
+    public String toString() {
+        return "{" + "\n"+
+                "\t'id': " + getId() + "\n"+
+                "\t'title': "+getTitle()+ "\n"+
+                "\t'duration': "+getDuration()+ "\n"+
+                "\t'rank': "+getRank()+ "\n"+
+                "\t'releaseDate': "+getReleaseDate()+ "\n"+
+                "\t'preview': "+getPreview()+ "\n"+
+                "}";
+    }
+}
--- Labs/DeezerMusicQuiz/src/Main.java
+++ Labs/DeezerMusicQuiz/src/Main.java
@@ -1,6 +1,58 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        JFXPanel fxPanel = new JFXPanel();
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gs = gsonBuilder.create();
+
+        try {
+            DeezerTrack myTrack = gs.fromJson(deezerGET(), DeezerTrack.class);
+            System.out.println("## Reproducing music now ## " +myTrack.getPreview());
+            // System.out.println(myTrack.toString());
+            Media song = new Media(myTrack.getPreview());
+            MediaPlayer mediaPlayer = new MediaPlayer(song);
+            mediaPlayer.play();
+            Scanner input = new Scanner(System.in);
+            System.out.println("[IN] Input name of the song: ");
+            String titleGuess = input.nextLine();
+            if (checkTitle(titleGuess, myTrack))
+                System.out.println("[OK] Congratulations! You won!");
+            else
+                System.out.println("[ERR] Sorry, wrong answer.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String deezerGET() throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        String deezerTrackURL = "https://api.deezer.com/track/3135556";
+        Request request = new Request.Builder()
+                .url(deezerTrackURL).build();
+        Response response = okHttpClient.newCall(request).execute();
+        return response.body().string();
+    }
+
+    /**
+     * Function to check if the string input by the user is the actual title
+     * @param _title Input from the user - title
+     * @return True if it's the same title, False otherwise
+     */
+    private static Boolean checkTitle(String _title, DeezerTrack _myTrack){
+        return _title.compareTo(_myTrack.getTitle()) == 0;
     }
 }
